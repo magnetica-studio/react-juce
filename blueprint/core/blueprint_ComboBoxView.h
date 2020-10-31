777fadd6a502490b7e8111c6a86f57c65f3627d8
szk2s <suzukisjss@gmail.com>
2020-10-31 14:51:46 +0900

    apply formatter

--- blueprint/core/blueprint_ComboBoxView.h
+++ blueprint/core/blueprint_ComboBoxView.h
@@ -11,30 +11,26 @@
 #include "blueprint_View.h"
 
 
-namespace blueprint
-{
+namespace blueprint {
 
     //==============================================================================
     /** The ImageView class is a core view for drawing images within Blueprint's
         layout system.
      */
-    class ComboBoxView : public View
-    {
+    class ComboBoxView : public View {
     public:
         //==============================================================================
         ComboBoxView()
-            : View()
-        {
+                : View() {
             addAndMakeVisible(comboBox, 1);
-            comboBox.addItem("Plain",  1);
-            comboBox.addItem("Bold",   2);
+            comboBox.addItem("Plain", 1);
+            comboBox.addItem("Bold", 2);
             comboBox.addItem("Italic", 3);
             comboBox.setSelectedId(1);
             comboBox.setLookAndFeel(&lookAndFeel);
         }
 
-        void setProperty(const juce::Identifier &name, const juce::var &value) override
-        {
+        void setProperty(const juce::Identifier &name, const juce::var &value) override {
             // TODO: Paintメソッドに移した方が良さそう。Hot-reload時にクラッシュする
             if (name == juce::StringRef("background-color"))
                 comboBox.setColour(
@@ -66,20 +62,17 @@ namespace blueprint
                         juce::ComboBox::ColourIds::outlineColourId,
                         juce::Colour::fromString(value.toString())
                 );
-            if (name == juce::StringRef("popup-color"))
-            {
+            if (name == juce::StringRef("popup-color")) {
                 lookAndFeel.setColour(
                         juce::PopupMenu::ColourIds::backgroundColourId,
                         juce::Colour::fromString(value.toString()));
             }
-            if (name == juce::StringRef("highlight-color"))
-            {
+            if (name == juce::StringRef("highlight-color")) {
                 lookAndFeel.setColour(
                         juce::PopupMenu::ColourIds::highlightedBackgroundColourId,
                         juce::Colour::fromString(value.toString()));
             }
-            if (name == juce::StringRef("highlight-background-color"))
-            {
+            if (name == juce::StringRef("highlight-background-color")) {
                 lookAndFeel.setColour(
                         juce::PopupMenu::ColourIds::highlightedTextColourId,
                         juce::Colour::fromString(value.toString()));
@@ -88,11 +81,11 @@ namespace blueprint
             View::setProperty(name, value);
         }
 
-        void resized() override
-        {
-            comboBox.setBounds(0, 0, getWidth() , getHeight());
+        void resized() override {
+            comboBox.setBounds(0, 0, getWidth(), getHeight());
             View::resized();
         }
+
     private:
         //==============================================================================
         juce::ComboBox comboBox;
